e0ef4f5c88ed57c9237070f458b0b19bd99e1d13
Rob Hern <robertjhern@gmail.com>
2017-03-14 13:15:48 +0000

    adding hamburger and responsive portfolio pieces

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-});
+
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
@@ -14,32 +14,47 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
+var bio = {
+    "jobtitle" : "Junior Front End Web Developer"
+
+}
+$("#jobtitle").html(bio.jobtitle);
+
+
 /*  RESPONSIVE PORTFOLIO BOXES */
 
 var portfolio = {
-  "projects" : [{
-    "mainimg" : "jacksite_small.jpg",
-    "name" : "3D Artist Site",
-    "for" : "Jack Mudge",
-    "description" : "Responsive portfolio site for an artist",
-    "morelink" : "/jackmudge.html",
-    "extlink" : "http://robhern135.github.io/Jack-Mudge"
-    },{
-    "mainimg" : "responsiveresume_small.jpg",
-    "name" : "Responsive Resume",
-    "for" : "Udacity Nanodegree",
-    "description" : "Resume populated on pageload with data from JSON objects",
-    "morelink" : "/responsiveresume.html",
-    "extlink" : "http://robhern135.github.io/responsive-resume"
-    },
-  ]
-};
+  "projects": [{
+        "mainimg": "img/jack2_small.jpg",
+        "name": "3D Artist Site",
+        "for": "Jack Mudge",
+        "morelink": "jackmudge.html",
+        "extlink": "http://robhern135.github.io/Jack-Mudge"
+    }, {
+        "mainimg": "img/resume1_small.png",
+        "name": "TEST TEST TEST",
+        "for": "Udacity Nanodegree",
+        "morelink": "responsiveresume.html",
+        "extlink": "http://robhern135.github.io/responsive-resume"
+    }, {
+        "mainimg": "http://placekitten.com/1000/500",
+        "name": "TEST TEST TEST",
+        "for": "TEST",
+        "morelink": "http://google.com",
+        "extlink": "http://google.co.uk"
+    }]
+}
 
 var HTMLprojectStart = '<div class="project-box"></div>';
-var HTMLprojectMainImg = '<img src="%data%"/>';
+var HTMLprojectMainImg = '<img src="%data%" />';
+//replace with:
+//<img src="img/jack1_small.jpg"
+//      onmouseover="this.src='img/jack2_small.jpg'"
+//      onmouseout="this.src='img/jack1_small.jpg'" 
+//      alt ="Jack Mudge - 3D Artist Portfolio Site"/>
 var HTMLprojectName = '<h3>%data%</h3>';
-var HTMLprojectFor = '<h4 class="handwritten">date-text">%data%</h4>';
-var HTMLprojectMorelink = '<p><a href="&data&">Learn More</a>  |  ';
+var HTMLprojectFor = '<h4 class="handwritten">%data%</h4>';
+var HTMLprojectMorelink = '<p><a href="%data%">Learn More</a>  |  ';
 var HTMLprojectExtlink = '<a href="%data%">View</a></p>';
 
 
@@ -61,3 +76,5 @@ portfolio.display = function() {
 }; //end of function
 
 portfolio.display() ;
+
+});
