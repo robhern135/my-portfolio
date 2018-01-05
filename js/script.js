773b5fd53b9e7e35126f54deea90845629cc7867
Rob Hern <robertjhern@gmail.com>
2018-01-05 14:12:14 +0000

    adding media 10 showcase

--- js/script.js
+++ js/script.js
@@ -103,17 +103,37 @@ var bio = {
 $("#jobtitle").html(bio.jobtitle);
 
 
+var header = $('#m10_header');
+var range = 200;
+
+$(window).on('scroll', function () {
+  
+  var scrollTop = $(this).scrollTop(),
+      height = $(window).outerHeight(),
+      offset = height / 2,
+      calc = 1 - (scrollTop - offset + range) / range;
+
+  header.css({ 'opacity': calc });
+
+  if (calc > '1') {
+    header.css({ 'opacity': 1 });
+  } else if ( calc < '0' ) {
+    header.css({ 'opacity': 0 });
+  }
+  
+});
+
 
 
 //remove headings on screens less than 1300px //
-//initialise stacking table
-  if ( $(window).width() < 1300) {      
-    $('.slide-header').css('display','none');
+// //initialise stacking table
+//   if ( $(window).width() < 1300) {      
+//     $('.slide-header').css('display','none');
 
-  } 
-  else {
-    console.log("screen is too large for stackable table");
-  }
+//   } 
+//   else {
+//     console.log("screen is too large for stackable table");
+//   }
 
 // PUT EVERYTHING ABOVE HERE
 });
